a0261c446daeb906a7039d2fa65b23ae2b654d93
Abdul Majid <abdulmajidcse@gmail.com>
2023-08-07 00:17:42 +0600

    update login and others page component

--- resources/js/components/auth/header/VisitSiteLink.tsx
+++ resources/js/components/auth/header/VisitSiteLink.tsx
@@ -7,7 +7,7 @@ export default function VisitSiteLink() {
         <>
             <Tooltip content="Visit site">
                 <a
-                    href="/"
+                    href={import.meta.env.VITE_APP_URL}
                     className="inline-flex items-center p-2 text-base text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 >
                     <span className="sr-only">Visit site</span>

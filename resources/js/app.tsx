d0e5957b22d6f71c2d5848b47ed8b2d2bab44a60
Abdul Majid <abdulmajidcse@gmail.com>
2023-08-07 20:41:16 +0600

    essential library installed and redux and rtk query setup

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,9 +3,16 @@ import "./assets/css/main.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./AppRoutes.tsx";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <AppRoutes />
+        <Provider store={store}>
+            <AppRoutes />
+            <ToastContainer />
+        </Provider>
     </React.StrictMode>
 );
---
+++ resources/js/services/apiService.ts
@@ -0,0 +1,61 @@
+import { createApi } from "@reduxjs/toolkit/query/react";
+import type { BaseQueryFn } from "@reduxjs/toolkit/query";
+import axios from "axios";
+import type { AxiosRequestConfig, AxiosError } from "axios";
+
+const axiosBaseQuery =
+    (
+        { baseUrl }: { baseUrl: string } = { baseUrl: "" }
+    ): BaseQueryFn<
+        {
+            url: string;
+            method: AxiosRequestConfig["method"];
+            data?: AxiosRequestConfig["data"];
+            headers?: AxiosRequestConfig["headers"];
+            params?: AxiosRequestConfig["params"];
+        },
+        unknown,
+        unknown
+    > =>
+    async ({ url, method, data, headers, params }) => {
+        try {
+            const result = await axios({
+                url: baseUrl + url,
+                method,
+                data,
+                headers,
+                params,
+            });
+
+            return { data: result.data };
+        } catch (axiosError) {
+            let err = axiosError as AxiosError;
+            return {
+                error: {
+                    status: err.response?.status,
+                    data: err.response?.data || err.message,
+                },
+            };
+        }
+    };
+
+export const apiService = createApi({
+    reducerPath: "api",
+    baseQuery: axiosBaseQuery({
+        baseUrl: import.meta.env.VITE_APP_URL,
+    }),
+    tagTypes: ["Auth"],
+    endpoints: (build) => {
+        return {
+            login: build.mutation({
+                query: (data) => ({
+                    url: "/auth/login",
+                    method: "post",
+                    data,
+                }),
+            }),
+        };
+    },
+});
+
+export const { useLoginMutation } = apiService;
